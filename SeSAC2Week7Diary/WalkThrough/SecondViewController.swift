07d0cef38ed3a06f268dff058608f322905582cf
jaehoon <sjh22058@gmail.com>
2022-08-16 16:49:42 +0900

    Initial Commit

---
+++ SeSAC2Week7Diary/WalkThrough/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  SeSAC2Week7Diary
+//
+//  Created by Seo Jae Hoon on 2022/08/16.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ SeSAC2Week7Diary/WalkThrough/ThirdViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ThirdViewController.swift
+//  SeSAC2Week7Diary
+//
+//  Created by Seo Jae Hoon on 2022/08/16.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
