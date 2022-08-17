41c1212520ea30ba91bebe6f38dc2e96370b03bb
jaehoon <sjh22058@gmail.com>
2022-08-18 00:22:26 +0900

    stack view 사용 + 하단 button code 수정

--- SeSAC2Week7Diary/CodeBase/KakaoSnapCodeViewController.swift
+++ SeSAC2Week7Diary/CodeBase/KakaoSnapCodeViewController.swift
@@ -17,25 +17,33 @@ class KakaoSnapCodeViewController: UIViewController {
     }
     
     let xButton = UIButton().then {
-        $0.setImage(UIImage(systemName: "xmark"), for: .normal)
+        let size = UIImage.SymbolConfiguration(pointSize: 24)
+        let xImage = UIImage(systemName: "xmark", withConfiguration: size)
+        $0.setImage(xImage, for: .normal)
         $0.tintColor = .white
         $0.scalesLargeContentImage = true
     }
     
     let giftButton = UIButton().then {
-        $0.setImage(UIImage(systemName: "gift.circle.fill"), for: .normal)
+        let size = UIImage.SymbolConfiguration(pointSize: 24)
+        let giftImage = UIImage(systemName: "gift.circle.fill", withConfiguration: size)
+        $0.setImage(giftImage, for: .normal)
         $0.tintColor = .white
         $0.scalesLargeContentImage = true
     }
     
     let diceButton = UIButton().then {
-        $0.setImage(UIImage(systemName: "viewfinder.circle.fill"), for: .normal)
+        let size = UIImage.SymbolConfiguration(pointSize: 24)
+        let qrcodeImage = UIImage(systemName: "viewfinder.circle.fill", withConfiguration: size)
+        $0.setImage(qrcodeImage, for: .normal)
         $0.tintColor = .white
         $0.scalesLargeContentImage = true
     }
     
     let settingButton = UIButton().then {
-        $0.setImage(UIImage(systemName: "gearshape.circle.fill"), for: .normal)
+        let size = UIImage.SymbolConfiguration(pointSize: 24)
+        let settingImage = UIImage(systemName: "gearshape.circle.fill", withConfiguration: size)
+        $0.setImage(settingImage, for: .normal)
         $0.tintColor = .white
         $0.scalesLargeContentImage = true
     }
@@ -63,10 +71,9 @@ class KakaoSnapCodeViewController: UIViewController {
         $0.backgroundColor = .lightGray
     }
     
-    let chatButton = UIButton().then {
-        $0.setImage(UIImage(systemName: "message.fill"), for: .normal)
+    let chatImage = UIImageView().then {
+        $0.image = UIImage(systemName: "message.fill")
         $0.tintColor = .white
-        $0.scalesLargeContentImage = true
     }
     
     let chatLabel = UILabel().then {
@@ -76,10 +83,9 @@ class KakaoSnapCodeViewController: UIViewController {
         $0.font = .systemFont(ofSize: 13)
     }
     
-    let editButton = UIButton().then {
-        $0.setImage(UIImage(systemName: "pencil"), for: .normal)
+    let editImage = UIImageView().then {
+        $0.image = UIImage(systemName: "pencil")
         $0.tintColor = .white
-        $0.scalesLargeContentImage = true
     }
     
     let editLabel = UILabel().then {
@@ -89,10 +95,9 @@ class KakaoSnapCodeViewController: UIViewController {
         $0.font = .systemFont(ofSize: 13)
     }
     
-    let kakaoStoryButton = UIButton().then {
-        $0.setImage(UIImage(systemName: "quote.closing"), for: .normal)
+    let kakaoStoryImage = UIImageView().then {
+        $0.image = UIImage(systemName: "quote.closing")
         $0.tintColor = .white
-        $0.scalesLargeContentImage = true
     }
     
     let kakaoStoryLabel = UILabel().then {
@@ -102,19 +107,29 @@ class KakaoSnapCodeViewController: UIViewController {
         $0.font = .systemFont(ofSize: 13)
     }
     
+    let editButton = UIButton().then {
+        $0.setTitle("", for: .normal)
+    }
+    
+    let chatButton = UIButton().then {
+        $0.setTitle("", for: .normal)
+    }
+    
+    let kakaoStoryButton = UIButton().then {
+        $0.setTitle("", for: .normal)
+    }
+    
+    lazy var topStackView = UIStackView(arrangedSubviews: [giftButton, diceButton, settingButton]).then {
+        $0.axis = .horizontal
+        $0.spacing = 4
+        $0.distribution = .fillEqually
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(backgroundView)
         
-        backgroundView.snp.makeConstraints { make in
-            make.leading.equalToSuperview()
-            make.trailing.equalToSuperview()
-            make.top.equalToSuperview()
-            make.bottom.equalToSuperview()
-        }
-        
+        configureBackground()
         configureTopUI()
         configureMiddleUI()
         configureBottomUI()
@@ -125,63 +140,60 @@ class KakaoSnapCodeViewController: UIViewController {
         
     }
     
+    func configureBackground(){
+        
+        view.addSubview(backgroundView)
+        
+        backgroundView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        
+    }
+    
     
     // 오른쪽 3개 버튼 스택뷰
     func configureTopUI(){
         
-        
-        [xButton, giftButton, diceButton, settingButton].forEach { view.addSubview($0) }
-        
-        
+        [xButton, topStackView].forEach { backgroundView.addSubview($0) }
         
         xButton.snp.makeConstraints { make in
-            make.leftMargin.equalTo(20)
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
-            make.width.equalTo(view).multipliedBy(0.08)
+            make.left.equalTo(12)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.width.equalTo(view).multipliedBy(0.1)
             make.height.equalTo(xButton.snp.width)
         }
         
-        settingButton.snp.makeConstraints { make in
-            make.rightMargin.equalTo(-20)
-            make.top.equalTo(xButton.snp.top)
-            make.width.equalTo(xButton.snp.width)
+        topStackView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.right.equalTo(-12)
+            make.width.equalTo(view).multipliedBy(0.3)
             make.height.equalTo(xButton.snp.height)
         }
         
-        diceButton.snp.makeConstraints { make in
-            make.right.equalTo(settingButton.snp.left).offset(-20)
-            make.top.equalTo(xButton.snp.top)
-            make.width.equalTo(xButton.snp.width)
-            make.height.equalTo(xButton.snp.height)
-        }
-        
-        giftButton.snp.makeConstraints { make in
-            make.right.equalTo(diceButton.snp.left).offset(-20)
-            make.top.equalTo(xButton.snp.top)
-            make.width.equalTo(xButton.snp.width)
-            make.height.equalTo(xButton.snp.height)
-        }
     }
     
     // 가운데 들어가는 이미지, 레이블 2개 스택뷰
     func configureMiddleUI(){
         
-        [profileImageView, profileNameLabel, profileMessageLabel, lineView].forEach { view.addSubview($0) }
+        [profileImageView, profileNameLabel, profileMessageLabel, lineView].forEach { backgroundView.addSubview($0) }
         
         profileImageView.snp.makeConstraints { make in
             make.width.equalTo(view).multipliedBy(0.25)
             make.height.equalTo(profileImageView.snp.width).multipliedBy(1)
             make.centerX.equalToSuperview()
-            make.centerY.equalTo(view).offset(UIScreen.main.bounds.height / 5)
+            make.centerY.equalTo(view).offset(UIScreen.main.bounds.height / 5.5)
         }
         
         profileNameLabel.snp.makeConstraints { make in
-            make.top.equalTo(profileImageView.snp.bottom).offset(20)
+            make.top.equalTo(profileImageView.snp.bottom).offset(8)
             make.centerX.equalToSuperview()
         }
         
         profileMessageLabel.snp.makeConstraints { make in
-            make.top.equalTo(profileNameLabel.snp.bottom).offset(16)
+            make.top.equalTo(profileNameLabel.snp.bottom).offset(12)
             make.centerX.equalToSuperview()
         }
         
@@ -195,45 +207,65 @@ class KakaoSnapCodeViewController: UIViewController {
     
     func configureBottomUI(){
         
-        [editButton, editLabel, chatButton, chatLabel, kakaoStoryButton, kakaoStoryLabel].forEach { view.addSubview($0) }
         
-        editButton.snp.makeConstraints { make in
+        [editImage, editLabel, chatImage, chatLabel, kakaoStoryImage, kakaoStoryLabel].forEach { backgroundView.addSubview($0) }
+        
+        editImage.addSubview(editButton)
+        chatImage.addSubview(chatButton)
+        kakaoStoryImage.addSubview(kakaoStoryButton)
+        
+        
+        editImage.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.top.equalTo(lineView).offset(16)
-            make.width.equalTo(profileImageView).multipliedBy(0.33)
-            make.height.equalTo(editButton.snp.width)
+            make.top.equalTo(lineView).offset(24)
+            make.width.equalTo(profileImageView).multipliedBy(0.3)
+            make.height.equalTo(editImage.snp.width)
         }
         
         editLabel.snp.makeConstraints { make in
-            make.top.equalTo(editButton.snp.bottom).offset(8)
+            make.top.equalTo(editImage.snp.bottom).offset(8)
             make.centerX.equalToSuperview()
         }
         
-        chatButton.snp.makeConstraints { make in
-            make.width.equalTo(editButton.snp.width)
-            make.height.equalTo(editButton.snp.height)
-            make.right.equalTo(editButton.snp.left).offset(-80)
-            make.centerY.equalTo(editButton)
+        editButton.snp.makeConstraints { make in
+            make.top.equalTo(editImage.snp.top)
+            make.left.right.bottom.equalTo(editLabel)
+        }
+        
+        chatImage.snp.makeConstraints { make in
+            make.width.equalTo(editImage.snp.width)
+            make.height.equalTo(editImage.snp.height)
+            make.right.equalTo(editImage.snp.left).offset(-80)
+            make.centerY.equalTo(editImage)
         }
         
         chatLabel.snp.makeConstraints { make in
-            make.top.equalTo(chatButton.snp.bottom).offset(8)
-            make.centerX.equalTo(chatButton)
+            make.top.equalTo(chatImage.snp.bottom).offset(8)
+            make.centerX.equalTo(chatImage)
         }
         
-        kakaoStoryButton.snp.makeConstraints { make in
-            make.width.equalTo(editButton.snp.width)
-            make.height.equalTo(editButton.snp.height)
-            make.left.equalTo(editButton.snp.right).offset(80)
-            make.centerY.equalTo(editButton)
+        chatButton.snp.makeConstraints { make in
+            make.top.equalTo(chatImage.snp.top)
+            make.left.right.bottom.equalTo(chatLabel)
+        }
+        
+        kakaoStoryImage.snp.makeConstraints { make in
+            make.width.equalTo(editImage.snp.width)
+            make.height.equalTo(editImage.snp.height)
+            make.left.equalTo(editImage.snp.right).offset(80)
+            make.centerY.equalTo(editImage)
         }
         
         kakaoStoryLabel.snp.makeConstraints { make in
-            make.top.equalTo(kakaoStoryButton.snp.bottom).offset(8)
-            make.centerX.equalTo(kakaoStoryButton)
+            make.top.equalTo(kakaoStoryImage.snp.bottom).offset(8)
+            make.centerX.equalTo(kakaoStoryImage)
+        }
+        
+        kakaoStoryButton.snp.makeConstraints { make in
+            make.top.equalTo(kakaoStoryImage.snp.top)
+            make.left.right.bottom.equalTo(kakaoStoryLabel)
         }
         
     }
-    
 }
 
